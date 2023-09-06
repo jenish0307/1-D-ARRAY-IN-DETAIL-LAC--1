6de4565ee64726a733c2e63c397c45ceba7b1a9d
jenish0307 <131126665+jenish0307@users.noreply.github.com>
2023-09-06 21:21:36 +0530

    Add files via upload

---
+++ LENGTH OF 1 D ARRAY .C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    int length = sizeof(array) / sizeof(array[0]);
+
+    printf("Length of the array: %d\n", length);
+
+    return 0;
+}
